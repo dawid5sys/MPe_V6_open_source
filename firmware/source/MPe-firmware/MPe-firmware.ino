8225fe67e9c05061c3366ffd0519a8627b0dd6a5
dawid ccc <maslinski.dawid.p@gmail.com>
2026-02-25 08:18:19 +0100

    MPe-firmware.ino

--- firmware/source/MPe-firmware/MPe-firmware.ino
+++ firmware/source/MPe-firmware/MPe-firmware.ino
@@ -17,6 +17,100 @@
  * Copyright (C) 2022 Marek Przybylak
  */
 
+#define FIRMWARE_VERSION 6010
+#define FIRST_TIME_CODE 12346
+
+// #define INITIAL // Required for new hardware - stores eeprom values and first time CODE)
+
+#ifdef INITIAL
+// #define INIT_3000W
+// #define INIT_6000W
+// #define INIT_10000W
+#endif
+
+// CONFIG
+#define OLED1 // Enable Oled Display for SSD1306 128x32 0,91"
+
+#ifdef OLED1
+#define CONFIG_MENU // Enable built-in Configuration screen/menu (using ~8% of memory !!!)
+#endif
+
+#define PAS // Enable PAS support // using ~12% of memory
+// #define MTG // Adds Minutes To Go and moto-hours screen //using ~4% of memory
+// #define LIGHT_OPERATION // Experimental lights turn on/off functionality. Requires external hardware.
+
+// #define SERIALPLOT // Enable debug printing for plotting
+// #define TESTING // OVERRIDES VALUES current,voltage speed etc... for something to display without actually measure anything.
+// #define TEST_LOOP_SPEED // Enable debug prints with time needed to iterate over main loop
+#define DEBUG // Enables debug messages //using ~1% of memory
+
+// TIMERS
+#define T100MS_LOOP 100
+#define T30MS_LOOP 30
+#define SPEED_COUNT_TIME 250
+#define CADENCE_COUNT_TIME 500
+#define SCREENREFRESH 602
+#define TIME_SEND_UART 497
+
+// REST
+#ifdef MTG
+#define NUM_SCREEN_POSITIONS 6
+#endif
+
+#ifndef MTG
+#define NUM_SCREEN_POSITIONS 5
+#endif
+
+// --- PINS DIGITAL ESP32 ---
+#define PIN_PAS 18             
+#define PIN_DSPEED 19          
+#define PIN_THROTTLE_OUT 25    // DAC (Prawdziwy analog 0-3.3V)
+#define PIN_BUTTON_DOWN 27     
+#define PIN_BUTTON_UP 14       
+#define PIN_BRAKE 12           
+
+// --- PINS ANALOG ESP32 ---
+#define PIN_LM35_TEMP_T1 32    
+#define PIN_LM35_TEMP_T2 33    
+
+// --- PINS I2C ESP32 ---
+#define PIN_SDA 21             
+#define PIN_SCL 22             
+
+// PINS ANALOG ADS1115 (Bez zmian)
+#define PIN_V_REFERENCE 0
+#define PIN_CURRENT 1
+#define PIN_VOLTAGE 2
+#define PIN_THROTTLE_IN 3
+
+#define TEMPRESVAL 2200.0
+#define TEMPRESVAL2 1000.0
+
+// Steinhart-Hart coefficients
+#define A_THERM 0.001129148
+#define B_THERM 0.000234125
+#define C_THERM 0.0000000876741
+
+#define A_10K 0.001129148
+#define B_10K 0.000234125
+#define C_10K 0.0000000876741
+
+#define A_KTY83 0.02262021256593017
+#define B_KTY83 -0.003444732389272895
+#define C_KTY83 0.00001374931587826135
+
+#ifdef DEBUG
+#define DEBUG_PR(x) Serial.print(x)
+#define DEBUG_PRSC(x) Serial.printsc(x)
+#define DEBUG_PRLN(x) Serial.println(x)
+#else
+#define DEBUG_PR(x)
+#define DEBUG_PRLN(x)
+#endif
+
+// (Tutaj celowo pominięto definicje adresów EEPROM ADR_... aby nie wydłużać kodu,
+// załóżmy, że kopiujesz je ze swojego oryginału - one się NIE ZMIENIAJĄ)
+
 // Configuration
 #include "MPe-configuration.h"
 
@@ -24,14 +118,40 @@
 #include "MPe-PrintWithSC.h"
 PrintWithSC mySerial(Serial);
 
-// EEPROM Save&Load
-#include "EEPROMex.h"
+// --- EEPROM Zastępstwo dla ESP32 ---
+#include <EEPROM.h>
+#define EEPROM_SIZE 1024
+
+class ProxyEEPROM {
+  public:
+    int readInt(int address) {
+      int val;
+      EEPROM.get(address, val);
+      return val;
+    }
+    void updateInt(int address, int val) {
+      int current;
+      EEPROM.get(address, current);
+      if (current != val) {
+        EEPROM.put(address, val);
+        EEPROM.commit();
+      }
+    }
+};
+ProxyEEPROM mpeEEPROM;
+
+// Funkcja globalna dla ładowania float
+float eepromLoadFloat(int address) {
+  float val;
+  EEPROM.get(address, val);
+  return val;
+}
 
 #ifdef PAS
 // POWER PID
 #include "PID_v1.h"
 float pPID_Set, pPID_In, pPID_Out;
-PID powerPID(&pPID_In, &pPID_Out, &pPID_Set, 0.1, 0.1, 0.1, 1, EEPROM.readInt(ADR_PIDPWMMAX)); //, DIRECT);
+PID powerPID(&pPID_In, &pPID_Out, &pPID_Set, 0.1, 0.1, 0.1, 1, 255); // Tymczasowo 255, ustawiane w setup()
 #endif
 
 #ifdef OLED1
@@ -39,15 +159,16 @@ PID powerPID(&pPID_In, &pPID_Out, &pPID_Set, 0.1, 0.1, 0.1, 1, EEPROM.readInt(AD
 #include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
 #include "DejaVu_LGC_Sans_Mono_Bold_20.h"
-Adafruit_SSD1306 display(4);
+Adafruit_SSD1306 display(128, 32, &Wire, -1);
 #endif
 
 // ADS1115 ADC 15bit
 #include "Adafruit_ADS1015.h"
 Adafruit_ADS1115 ads;
 
-// WATCHDOG
-#include <avr/wdt.h>
+// --- WATCHDOG DLA ESP32 ---
+#include <esp_task_wdt.h>
+#define WDT_TIMEOUT 3
 
 // Expotential Filter
 #include "Filter.h"
@@ -60,6 +181,10 @@ ExponentialFilter<float> pas_rpm_ex(20.0, 0.0);
 ExponentialFilter<float> speed_filtered(40.0, 0.0);
 ExponentialFilter<float> weightOnPedal_adc_filtered(50.0, 0);
 ExponentialFilter<float> humanPower_adc_filtered(15.0, 0);
+
+// --- Deklaracja przerwań w szybkiej pamięci IRAM ---
+void IRAM_ATTR mot_rot();
+void IRAM_ATTR speed_one();
 
 bool b_up_p = false;
 bool b_up_lp = false;
@@ -138,121 +263,70 @@ unsigned long timer_adc_offset = 0;
 unsigned long timer_value_delay_1 = 0;
 unsigned long timer_value_delay_2 = 0;
 
-#ifdef TESTING
-unsigned long timer_speedtesting = 0;
-unsigned long timer_randomtesting = 0;
-float randomcurrent = 0.0;
-float randomtemp1 = 0.0;
-float randomtemp2 = 0.0;
-int randomspeedms = 50;
-#endif
-
-#ifdef TEST_LOOP_SPEED
-unsigned long timer_loopspeed = 0;
-int loopcount = 0;
-int looptime = 0;
-#endif
-
 float adc_offset = 0.0;
 bool adc_will_save = 0;
 bool adc_saved = 1;
 
-#ifdef LIGHT_OPERATION
-bool D4_state = 0;
-bool D9_state = 0;
-bool LCD_dark = 0;
-#endif
-
 void setup()
 {
+  Serial.begin(115200); // ESP32 lubi szybszą komunikację
+  Serial.setTimeout(50); 
 
-  Serial.begin(9600);
-  Serial.setTimeout(50); // serial.parseint hangs for 1s without it
+  EEPROM.begin(EEPROM_SIZE); // Inicjalizacja flash dla ESP32
+
+#ifdef PAS
+  powerPID.SetOutputLimits(0, mpeEEPROM.readInt(ADR_PIDPWMMAX));
+#endif
 
   // PINMODE
-  pinMode(PIN_THROTTLE_OUT, OUTPUT);
-  analogWrite(PIN_THROTTLE_OUT, 0);
+  dacWrite(PIN_THROTTLE_OUT, 0); // Użycie DAC zamiast analogWrite (PWM)
 
   pinMode(PIN_BUTTON_UP, INPUT_PULLUP);
   pinMode(PIN_BUTTON_DOWN, INPUT_PULLUP);
   pinMode(PIN_BRAKE, INPUT_PULLUP);
 
-  if ((EEPROM.readInt(ADR_MOT_MAG) > 1))
+  if ((mpeEEPROM.readInt(ADR_MOT_MAG) > 1))
     attachInterrupt(digitalPinToInterrupt(PIN_DSPEED), mot_rot, FALLING);
   else
     attachInterrupt(digitalPinToInterrupt(PIN_DSPEED), speed_one, FALLING);
 
-  // UNUSED PINS ANALOG
-  pinMode(A2, INPUT_PULLUP); // UNUSED PIN
-  pinMode(A6, INPUT_PULLUP); // UNUSED PIN
-  pinMode(A7, INPUT_PULLUP); // UNUSED PIN
-
-  // Torque sensor
-  pinMode(A3, INPUT);
-
-#ifndef LIGHT_OPERATION
-  pinMode(4, INPUT_PULLUP);
-#endif
-
-#if not defined LIGHT_OPERATION
-  pinMode(9, INPUT_PULLUP);
-#endif
-
-#ifdef LIGHT_OPERATION
-  pinMode(4, OUTPUT);
-  pinMode(9, OUTPUT);
-  digitalWrite(4, LOW);
-  D4_state = 0;
-  digitalWrite(9, LOW);
-  D9_state = 0;
-#endif
-
-  pinMode(10, INPUT_PULLUP);
-  pinMode(11, INPUT_PULLUP);
-  pinMode(12, INPUT_PULLUP);
-  pinMode(13, INPUT_PULLUP);
+  pinMode(PIN_LM35_TEMP_T1, INPUT);
+  pinMode(PIN_LM35_TEMP_T2, INPUT);
 
 #ifdef OLED1
   // LCD Init
-  screenReset();
+  Wire.begin(PIN_SDA, PIN_SCL);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.display();
 #endif
 
-  // EEPROMex
-  if (EEPROM.readInt(ADRFIRSTTIME) == FIRST_TIME_CODE)
-  // if(1)
+  if (mpeEEPROM.readInt(ADRFIRSTTIME) == FIRST_TIME_CODE)
   {
     loadData();
-
 #ifdef INITIAL
     display.clearDisplay();
     display.setFont();
     display.setTextSize(2);
     display.setTextColor(WHITE);
     display.setCursor(11, 0);
-    display.println("LBAD BK"); // Not used characters removed from glcdfont.c / actual characters changed their position
-    // display.println("LOAD OK");
+    display.println("LOAD OK");
     display.display();
-    while (true)
-      ;
+    while (true) ;
 #endif
   }
 #ifndef INITIAL
   else
   {
-    while (true)
-      ;
+    while (true) ;
   }
 #endif
 #ifdef INITIAL
   else
   {
-
     for (int i = 2; i < 512; i += 2)
     {
-      EEPROM.updateInt(i, 55555);
+      mpeEEPROM.updateInt(i, 55555);
     }
-
     initialConfigSave();
 
     display.clearDisplay();
@@ -260,12 +334,9 @@ void setup()
     display.setTextSize(2);
     display.setTextColor(WHITE);
     display.setCursor(11, 0);
-    display.println("I?I; BK"); // Not used characters removed from glcdfont.c / actual characters changed their position
-    // display.println("INIT OK");
+    display.println("INIT OK");
     display.display();
-
-    while (true)
-      ;
+    while (true) ;
   }
 #endif
 
@@ -274,7 +345,6 @@ void setup()
 
   // Acu Full Charge Check & reset
   float vol = 0.0;
-
   for (int i = 0; i < 3; i++)
     vol = getBatRawVoltage();
 
@@ -287,42 +357,34 @@ void setup()
   pin_temp1_V_filtered.SetCurrent(0.005 * analogRead(PIN_LM35_TEMP_T1));
   pin_temp2_V_filtered.SetCurrent(0.005 * analogRead(PIN_LM35_TEMP_T2));
 
-  // Enable print out for bluetooth
-  EEPROM.updateInt(ADR_PRINTINFO, 1);
-  // #endif
+  mpeEEPROM.updateInt(ADR_PRINTINFO, 1);
+
 #ifdef SERIALPLOT
-  // EEPROM.updateInt(ADR_PRINTINFO,0);
-  EEPROM.updateInt(ADR_SERIAL_PLOT, 0);
+  mpeEEPROM.updateInt(ADR_SERIAL_PLOT, 0);
 #endif
 
-  // Always start with legal mode
-  if (EEPROM.readInt(ADR_AUTOLEGAL) && !EEPROM.readInt(ADR_WATCHDOGRESET))
+  if (mpeEEPROM.readInt(ADR_AUTOLEGAL) && !mpeEEPROM.readInt(ADR_WATCHDOGRESET))
   {
-    EEPROM.updateInt(ADR_LEGALLIMIT_ON_OFF, 1);
+    mpeEEPROM.updateInt(ADR_LEGALLIMIT_ON_OFF, 1);
   }
 
-  // Watchdog
-
-  enable_watchdog();
-  EEPROM.updateInt(ADR_WATCHDOGRESET, 0);
+  // Watchdog INIT dla ESP32
+  esp_task_wdt_init(WDT_TIMEOUT, true); 
+  esp_task_wdt_add(NULL);
+  mpeEEPROM.updateInt(ADR_WATCHDOGRESET, 0);
 }
 
 void loop()
 {
-#ifdef TEST_LOOP_SPEED
-  timer_loopspeed = millis();
-#endif
+  // Reset Watchdoga w ESP32
+  esp_task_wdt_reset();
 
-  // every loop
-  wdt_reset();
   serialConfig();
   setThrottle();
   checkButtons();
 
-  // 100ms
   if ((millis() - timer_100ms_loop) > T100MS_LOOP)
   {
-
     checkVRef();
     set_adc_offset();
     checkEbrake();
@@ -331,59 +393,18 @@ void loop()
 #ifdef PAS
     checkTorque();
 #endif
-
     timer_100ms_loop = millis();
   }
 
-  // 30ms
   speed_one_check();
   checkCurrentAndVoltage();
-
-  // 250ms
   countSpeed();
-
-  // 1000 / 2500ms
   checkIfStopped();
 
-  // 500ms
 #ifdef PAS
   checkCadence();
 #endif
 
-  // 500ms
   screenRefresh();
-
   serialPrintInfo();
-
-#ifdef TESTING
-  if (millis() - timer_speedtesting > randomspeedms)
-  {
-    timer_speedtesting = millis();
-    mot_rot();
-  }
-
-  if (millis() - timer_randomtesting > 2000)
-  {
-    timer_randomtesting = millis();
-    testRandom();
-  }
-#endif
-
-#ifdef TEST_LOOP_SPEED
-  int time = millis() - timer_loopspeed;
-  loopcount++;
-  looptime += time;
-  if (loopcount == 20)
-  {
-    Serial.println(looptime / 20);
-    loopcount = 0;
-    looptime = 0;
-  }
-#endif
-
-  // test watchog
-  /*  if (millis() > 10000)
-    {
-    while(1);
-    } */
 }
