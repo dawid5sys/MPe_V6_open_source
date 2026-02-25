18a3fe4efdeef0922301c2ce1ca1982ba9941140
dawid ccc <maslinski.dawid.p@gmail.com>
2026-02-25 12:05:00 +0100

    MPe-temperature.ino

--- firmware/source/MPe-firmware/MPe-temperature.ino
+++ firmware/source/MPe-firmware/MPe-temperature.ino
@@ -42,15 +42,16 @@ int Convert_VtoTempThermistor(float v_in, int type) //[V]
     coef_C = C_KTY83;
   }
 
-  float Rs = ((TEMPRESVAL * (vRef - 0.67)) / v_in) - TEMPRESVAL; // resistance of thermistor
+  // ESP32: Termistory zasilane z 3.3V, stary kod używał (vRef - 0.67) dla 5V. 
+  // Upraszczamy to pod czyste zasilanie 3.3V bez spadków na diodach
+  float supplyV = 3.3; 
+  float Rs = ((TEMPRESVAL * supplyV) / v_in) - TEMPRESVAL; // resistance of thermistor
 
   if (type == 3 || type == 4)
-    Rs = TEMPRESVAL / ((((vRef / v_in) - 1.0) * (TEMPRESVAL / TEMPRESVAL2)) - 1.0); // Resistance of thermistor Mxus3k Turbo (common ground with hall)
+    Rs = TEMPRESVAL / ((((supplyV / v_in) - 1.0) * (TEMPRESVAL / TEMPRESVAL2)) - 1.0); 
 
   // T = 1/(a + b×ln(Rs) + c×(ln(Rs))3 )  http://www.useasydocs.com/theory/ntc.htm
-
   float lnRs = log(Rs);
-
   float temp = (1 / (coef_A + (coef_B * lnRs) + (coef_C * (lnRs * lnRs * lnRs)))) - 273.15;
 
   return int(temp);
@@ -59,7 +60,7 @@ int Convert_VtoTempThermistor(float v_in, int type) //[V]
 int getTemp_1()
 {
 #ifndef TESTING
-  return getTemp(pin_temp1_V_filtered.Current(), EEPROM.readInt(ADR_TEMPTYPE1));
+  return getTemp(pin_temp1_V_filtered.Current(), mpeEEPROM.readInt(ADR_TEMPTYPE1));
 #endif
 #ifdef TESTING
   return randomtemp1;
@@ -69,7 +70,7 @@ int getTemp_1()
 int getTemp_2()
 {
 #ifndef TESTING
-  return getTemp(pin_temp2_V_filtered.Current(), EEPROM.readInt(ADR_TEMPTYPE2));
+  return getTemp(pin_temp2_V_filtered.Current(), mpeEEPROM.readInt(ADR_TEMPTYPE2));
 #endif
 #ifdef TESTING
   return randomtemp2;
@@ -85,7 +86,7 @@ int getTemp(float tempPinV, int type)
   else
     temp = Convert_VtoTempThermistor(tempPinV, type);
 
-  if (EEPROM.readInt(ADR_TEMPCTEMPF))
+  if (mpeEEPROM.readInt(ADR_TEMPCTEMPF))
     return convertCtoF(temp);
   else
     return temp;
@@ -93,20 +94,21 @@ int getTemp(float tempPinV, int type)
 
 void checkTemp()
 {
-  float pin_temp_V = vRef / 1024.0 * analogRead(PIN_LM35_TEMP_T1);
+  // ESP32: 3.3V referencyjne i rozdzielczość 4095 zamiast 1024
+  float pin_temp_V = 3.3 / 4095.0 * analogRead(PIN_LM35_TEMP_T1);
   pin_temp1_V_filtered.Filter(pin_temp_V);
 
-  pin_temp_V = vRef / 1024.0 * analogRead(PIN_LM35_TEMP_T2);
+  pin_temp_V = 3.3 / 4095.0 * analogRead(PIN_LM35_TEMP_T2);
   pin_temp2_V_filtered.Filter(pin_temp_V);
 
-  if ((getTemp_1() > int(EEPROM.readInt(ADR_OVHT1)) || getTemp_2() > int(EEPROM.readInt(ADR_OVHT2))) && temperatureOK)
+  if ((getTemp_1() > int(mpeEEPROM.readInt(ADR_OVHT1)) || getTemp_2() > int(mpeEEPROM.readInt(ADR_OVHT2))) && temperatureOK)
   {
     temperatureOK = false;
   }
 
   if (!temperatureOK)
   {
-    if (getTemp_1() < int(EEPROM.readInt(ADR_OVHT1)) - 10 && getTemp_2() < int(EEPROM.readInt(ADR_OVHT2)) - 10)
+    if (getTemp_1() < int(mpeEEPROM.readInt(ADR_OVHT1)) - 10 && getTemp_2() < int(mpeEEPROM.readInt(ADR_OVHT2)) - 10)
       temperatureOK = true;
   }
 }
